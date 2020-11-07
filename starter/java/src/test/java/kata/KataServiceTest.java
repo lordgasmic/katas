f05e7d8666d103db7691a20016668b4aba19c73c
Nick Wellman <wellman.nicholas@gmail.com>
2020-11-07 16:42:48 -0500

    add tests for third scenario

--- starter/java/src/test/java/kata/KataServiceTest.java
+++ starter/java/src/test/java/kata/KataServiceTest.java
@@ -9,6 +9,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.constraints.AssertTrue;
+import java.util.List;
+
 public class KataServiceTest {
 
     private KataService service;
@@ -44,5 +47,32 @@ public class KataServiceTest {
 
         // assert that the time when the first message was posted is older than the second message
         assertTrue(service.getMessages("Bob").get(0).getTime().isBefore(service.getMessages("Bob").get(1).getTime()));
+    }
+
+    @Test
+    public void testGetAggregatedList() {
+        service.addMessageToTimeline("Alice", "I love the weather today.");
+        service.addMessageToTimeline("Bob", "Darn! We lost!");
+        service.addMessageToTimeline("Bob", "Good game though.");
+        service.addMessageToTimeline("Charlie", "I'm in New York today! Anyone wants to have a coffee?");
+
+        service.addFollower("Charlie", "Alice");
+        service.addFollower("Charlie", "Bob");
+
+        List<MessageDetail> messageDetails = service.viewWall("Charlie");
+
+        assertEquals("I love the weather today.", messageDetails.get(0).getMessage());
+        assertEquals("Darn! We lost!", messageDetails.get(1).getMessage());
+        assertEquals("Good game though.", messageDetails.get(2).getMessage());
+        assertEquals("I'm in New York today! Anyone wants to have a coffee?", messageDetails.get(3).getMessage());
+
+        assertTrue(messageDetails.get(0).getTime().isBefore(messageDetails.get(1).getTime()));
+        assertTrue(messageDetails.get(1).getTime().isBefore(messageDetails.get(2).getTime()));
+        assertTrue(messageDetails.get(2).getTime().isBefore(messageDetails.get(3).getTime()));
+
+        assertEquals("Alice", messageDetails.get(0).getUser());
+        assertEquals("Bob", messageDetails.get(1).getUser());
+        assertEquals("Bob", messageDetails.get(2).getUser());
+        assertEquals("Charlie", messageDetails.get(3).getUser());
     }
 }

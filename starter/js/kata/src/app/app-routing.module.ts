115b82c654f82d000cd6e088484753bbdc1db749
Nick Wellman <wellman.nicholas@gmail.com>
2020-11-09 10:42:35 -0500

    git header working

--- starter/js/kata/src/app/app-routing.module.ts
+++ starter/js/kata/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {MessageComponent} from './message/message.component';
+import {WallComponent} from './wall/wall.component';
+import {TimelineComponent} from './timeline/timeline.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'message',
+    component: MessageComponent,
+  },
+  {
+    path: 'wall',
+    component: WallComponent,
+  },
+  {
+    path: 'timeline',
+    component: TimelineComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

a44dd121c39efb9d1060e893859b89bb5f9edc70
Nick Wellman <wellman.nicholas@gmail.com>
2020-11-09 10:36:32 -0500

    add components i think i'm going to need

--- starter/js/kata/src/app/app.module.ts
+++ starter/js/kata/src/app/app.module.ts
@@ -3,9 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WallComponent } from './wall/wall.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { MessageComponent } from './message/message.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, WallComponent, TimelineComponent, MessageComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
